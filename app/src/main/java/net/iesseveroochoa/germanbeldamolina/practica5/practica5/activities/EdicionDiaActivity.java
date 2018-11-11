ca5caa99dc5d6f8cddfce26b44f003b586758917
merjan <merjan@gmail.com>
2018-11-11 20:00:25 +0100

    Activity edicion dia 2

--- app/src/main/java/net/iesseveroochoa/germanbeldamolina/practica5/practica5/activities/EdicionDiaActivity.java
+++ app/src/main/java/net/iesseveroochoa/germanbeldamolina/practica5/practica5/activities/EdicionDiaActivity.java
@@ -4,17 +4,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import net.iesseveroochoa.germanbeldamolina.practica5.practica5.R;
 
 public class EdicionDiaActivity extends AppCompatActivity {
+
+    private Spinner spn_valoracion;
+    private Button bt_calendario;
+    private TextView tv_fecha;
+    private EditText et_resumendia;
+    private EditText et_cuentadia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edicion_dia);
 
+        spn_valoracion = findViewById(R.id.spn_valorarvida);
+        bt_calendario =findViewById(R.id.bt_calendario);
+        tv_fecha = findViewById(R.id.tv_fecha);
+        et_resumendia = findViewById(R.id.et_resumendia);
+        et_cuentadia = findViewById(R.id.et_cuentadia);
     }
+
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_guardar, menu);
